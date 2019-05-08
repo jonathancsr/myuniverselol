48d5329b24400ace06bb5209a2b7d32b546104ce
joanthancsrr <joanthan@logpyx.com>
2019-05-08 10:37:16 -0300

    Updates

---
+++ precache-manifest.4a12259095ff32e5589b2662cdeb8b18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "590b30fff567f30bbe4ca526ab3e7cee",
+    "url": "/myuniverselol/index.html"
+  },
+  {
+    "revision": "801d6407ec15d9bccb32",
+    "url": "/myuniverselol/static/css/main.0e57732d.chunk.css"
+  },
+  {
+    "revision": "a90b0a2e27aaadd5337d",
+    "url": "/myuniverselol/static/js/2.68b98689.chunk.js"
+  },
+  {
+    "revision": "801d6407ec15d9bccb32",
+    "url": "/myuniverselol/static/js/main.6fe5bb29.chunk.js"
+  },
+  {
+    "revision": "7f56d7b8d1527945dab8",
+    "url": "/myuniverselol/static/js/runtime~main.c76ee058.js"
+  },
+  {
+    "revision": "8eccce155090104c7902b964cd0e12fe",
+    "url": "/myuniverselol/static/media/Friz Quadrata TT.8eccce15.ttf"
+  },
+  {
+    "revision": "194815fb84218c30f31bd793a7657a29",
+    "url": "/myuniverselol/static/media/img1.194815fb.jpg"
+  },
+  {
+    "revision": "3388e9ad13adb82f294fbc3d3ee4ad65",
+    "url": "/myuniverselol/static/media/img8.3388e9ad.jpg"
+  },
+  {
+    "revision": "d83d1811144a92d67e0376c56da5940c",
+    "url": "/myuniverselol/static/media/sky.d83d1811.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82733abd8012506db1e487b2e195518d.js"
+  "/myuniverselol/precache-manifest.4a12259095ff32e5589b2662cdeb8b18.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myuniverselol/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
