56f081b7980ad61ac9637b98d4fa7a3d100f5777
joanthancsrr <joanthan@logpyx.com>
2019-05-08 10:28:54 -0300

    alpha-0.1

---
+++ precache-manifest.82733abd8012506db1e487b2e195518d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076fb331f469b83640f7ea2402017768",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ec87a0cab743cf894eb",
+    "url": "/static/css/main.41954555.chunk.css"
+  },
+  {
+    "revision": "a90b0a2e27aaadd5337d",
+    "url": "/static/js/2.68b98689.chunk.js"
+  },
+  {
+    "revision": "6ec87a0cab743cf894eb",
+    "url": "/static/js/main.51a4371d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8eccce155090104c7902b964cd0e12fe",
+    "url": "/static/media/Friz Quadrata TT.8eccce15.ttf"
+  },
+  {
+    "revision": "194815fb84218c30f31bd793a7657a29",
+    "url": "/static/media/img1.194815fb.jpg"
+  },
+  {
+    "revision": "3388e9ad13adb82f294fbc3d3ee4ad65",
+    "url": "/static/media/img8.3388e9ad.jpg"
+  },
+  {
+    "revision": "d83d1811144a92d67e0376c56da5940c",
+    "url": "/static/media/sky.d83d1811.png"
+  }
+]);
