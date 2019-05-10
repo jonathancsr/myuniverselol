19ed30d280c47a556425010de4eb854e580fabe6
jonathan <jonathancsrr@gmail.com>
2019-05-09 22:18:59 -0300

    Updates

---
+++ precache-manifest.6917806a841797c8519933893e569607.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363824831d27f7560fb6d443e2ae9bfa",
+    "url": "/myuniverselol/index.html"
+  },
+  {
+    "revision": "0787e8e653af451b95c7",
+    "url": "/myuniverselol/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "517f922d70333b16563f",
+    "url": "/myuniverselol/static/css/main.9a8a28e3.chunk.css"
+  },
+  {
+    "revision": "0787e8e653af451b95c7",
+    "url": "/myuniverselol/static/js/2.19389659.chunk.js"
+  },
+  {
+    "revision": "517f922d70333b16563f",
+    "url": "/myuniverselol/static/js/main.594134fe.chunk.js"
+  },
+  {
+    "revision": "7f56d7b8d1527945dab8",
+    "url": "/myuniverselol/static/js/runtime~main.c76ee058.js"
+  },
+  {
+    "revision": "8eccce155090104c7902b964cd0e12fe",
+    "url": "/myuniverselol/static/media/Friz Quadrata TT.8eccce15.ttf"
+  },
+  {
+    "revision": "194815fb84218c30f31bd793a7657a29",
+    "url": "/myuniverselol/static/media/img1.194815fb.jpg"
+  },
+  {
+    "revision": "3388e9ad13adb82f294fbc3d3ee4ad65",
+    "url": "/myuniverselol/static/media/img8.3388e9ad.jpg"
+  },
+  {
+    "revision": "d83d1811144a92d67e0376c56da5940c",
+    "url": "/myuniverselol/static/media/sky.d83d1811.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//precache-manifest.12063809cb82321ae6c3eb022a874751.js"
+  "/myuniverselol/precache-manifest.6917806a841797c8519933893e569607.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/myuniverselol/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
